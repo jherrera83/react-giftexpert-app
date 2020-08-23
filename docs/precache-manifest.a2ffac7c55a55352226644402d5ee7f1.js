40117b5d85adfa52e3c7ef3b7f70a11f09a63b95
Julio Herrera <jeherrera@vectoritcgroup.com>
2020-08-23 12:31:19 -0500

    docs add

---
+++ docs/precache-manifest.a2ffac7c55a55352226644402d5ee7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007d79104a594835b05621d47740ea63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a3407469db2272fdccc",
+    "url": "/static/css/main.7575dde5.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3407469db2272fdccc",
+    "url": "/static/js/main.882150a7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
